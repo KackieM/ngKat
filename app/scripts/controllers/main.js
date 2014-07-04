5676929d8b7edc5baa6cb8ac7dee028c76e9dc4a
Katherine Mattison <kbmedisto@gmail.com>
2014-07-03 20:55:57 -0400

    got the customers controller called main ctrl working YESSSSSSSSSSS

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,21 +9,12 @@
  */
 angular.module('ngKatApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+    $scope.customers = [
+      {name:'John Smith',city: 'Phoenix'}, 
+      {name:'Katherine Mattison',city: 'NYC'}, 
+      {name:'Vinnie Kitts',city: 'Chicago'}, 
+      {name:'Jack Mattison',city: 'Charleston'}
     ];
 
-    <script>
-    	function ProductsCtrl ($scope) {
-
-    		$scope.products = [
-    			{name:'Ball of Fire',price: '$2000'}, 
-    			{name:'Fiery Umbrella',price: '$3000'}, 
-    			{name:'Kissing The Lipless',price: '$1000'}, 
-    			{name:'Lonely Queen',price: '$5000'}
-
-    		],
-    	}
+    
   });
