5a524346e215b77b5a1939785fc1f7418b2f8128
Katherine Mattison <kbmedisto@gmail.com>
2014-07-03 22:15:53 -0400

    trying to add new customer controller

---
+++ app/scripts/controllers/customers.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ngKatApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ngKatApp
+ */
+angular.module('ngKatApp')
+    .controller('NewCustCtrl', function ($scope) {
+    		$scope.products = [
+    			{name:'Ball of Fire',price: '$2000'}, 
+    			{name:'Fiery Umbrella',price: '$3000'}, 
+    			{name:'Kissing The Lipless',price: '$1000'}, 
+    			{name:'Lonely Queen',price: '$5000'}
+
+    		];
+  });
